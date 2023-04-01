2abb2bb1793cc80852f8c216d6b6ad3cd7c94962
T8RIN <oolbp@bk.ru>
2023-04-01 19:26:35 +0300

    Initial commit

---
+++ dynamic_theme/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.cookhelper.dynamic.theme"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.2"
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.palette:palette:1.0.0")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.0")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.t8rin"
+            artifactId = "dynamictheme"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
