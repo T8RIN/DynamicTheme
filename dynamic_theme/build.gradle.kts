ab263af81eea75a5e915b2351ee299354ac2cb8c
T8RIN <oolbp@bk.ru>
2023-04-12 15:05:59 +0300

    fix jitpack

--- dynamic_theme/build.gradle.kts
+++ dynamic_theme/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -38,13 +39,33 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.2"
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.01.00"))
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.palette:palette:1.0.0")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.0")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.t8rin"
+            artifactId = "dynamictheme"
+            version = "1.0.3"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
